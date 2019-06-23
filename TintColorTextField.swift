dce95a15d777b439802514ea2851ffbdb0c3ffc3
Mohit Gorakhiya <mohitgorakhiya@Mohits-MacBook-Pro.local>
2019-06-23 19:19:58 +0530

    second commit

---
+++ TintColorTextField.swift
@@ -0,0 +1,20 @@
+//
+//  TintColorTextField.swift
+//  TintColorTextField
+//
+//  Created by Mohit Gorakhiya on 6/23/19.
+//
+
+import UIKit
+
+class TintColorTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
